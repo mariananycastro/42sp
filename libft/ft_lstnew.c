4f008783ddac33bf68a8937a8d9b57b8f646b3b2
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-04-19 18:08:26 -0300

    bonus

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 12:11:29 by mariana           #+#    #+#             */
+/*   Updated: 2022/04/17 15:52:32 by mariana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstnew(void *content)
+{
+	t_list *new_content;
+
+	new_content = malloc(sizeof(t_list));
+	new_content->content = content;
+	new_content->next = NULL;
+	return (new_content);
+}
