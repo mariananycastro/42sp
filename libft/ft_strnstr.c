f4934838234aff5ae133f09813a345fdfa251d9f
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-05-05 20:03:29 -0300

    ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 19:15:58 by mariana           #+#    #+#             */
-/*   Updated: 2022/04/21 22:46:17 by mariana          ###   ########.fr       */
+/*   Updated: 2022/05/05 20:03:04 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,17 @@ char	*ft_strnstr(const char	*big, const char *little, size_t len)
 {
 	size_t	b;
 	size_t	l;
-	size_t	size_little;
 
 	b = 0;
-	l = 0;
-	size_little = ft_strlen(little);
-	if (size_little == 0)
-		return ((char *)(big));
-	while (b < len && little[l])
+	if (!little[b])
+		return ((char *) big);
+	while (big[b])
 	{
-		while (little[l] == big[b] && b < len)
-		{
-			if (l == size_little - 1)
-				return ((char *)big + b - l);
-			l++;
-			b++;
-		}
 		l = 0;
-		if (little[l] == big[b])
+		while (big[b + l] == little[l] && little[l] && b + l < len)
 			l++;
+		if (!little[l])
+			return ((char *) big + b);
 		b++;
 	}
 	return (NULL);
