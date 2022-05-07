f44c20b3d661433064f87fb8485647d0ad808bd6
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-05-06 21:11:05 -0300

    ft_putnbr_fd

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 20:13:46 by mariana           #+#    #+#             */
-/*   Updated: 2022/05/05 21:25:16 by mariana          ###   ########.fr       */
+/*   Updated: 2022/05/06 21:10:49 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*new_string;
+	long int	copy_n;
 
-	new_string = ft_itoa(n);
-	if (new_string)
-		write(fd, new_string, ft_strlen(new_string));
+	copy_n = n;
+	if (copy_n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		copy_n = copy_n * -1;
+	}
+	if (copy_n >= 10)
+		ft_putnbr_fd((copy_n / 10), fd);
+	ft_putchar_fd((copy_n % 10 + 48), fd);
 }
+
