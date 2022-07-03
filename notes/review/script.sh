efb9a926b9e5215bb2e4aa4f47381757bc7001ac
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-07-03 13:15:57 -0300

    borntoberoot script

---
+++ notes/review/script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+wall
+
+$'#Architecture: ' `hostnamectl | grep "Operating System" | cut -d ' ' -f5- ` '/' `hostnamectl | grep "Kernel" | cut -d ' ' -f14-`
+$'\n#CPU physical: '`cat /proc/cpuinfo | grep processor | wc -l` \
+--->???$'\n#vCPU:  '`cat /proc/cpuinfo | grep processor | wc -l` \ # code id???
+$'\n'`free -mega | awk 'NR==2{printf "#Memory Usage: %s/%sMB (%.2f%%)", $3,$2,$3*100/$2 }'` \
+$'\n'`df -h | awk '$NF=="/"{printf "#Disk Usage: %d/%dGB (%s)", $3,$2,$5}'` \
+-->$'\n'`top -bn1 | grep load | awk '{printf "#CPU Load: %.2f\n", $(NF-2)}'` \
+$'\n#Last boot: ' `who -b | awk '{print $3" "$4" "$5}'` \
+$'\n#LVM use: ' `lsblk |grep lvm | awk '{if ($1) {print "yes";exit;} else {print "no"} }'` \
+$'\n#Connection TCP:' `ss -t | grep ESTAB | wc -l ` 'ESTABLISHED'\
+$'\n#User log: ' `who | cut -d " " -f 1 | sort -u | wc -l` \
+$'\nNetwork: IP ' `hostname -I`"("`ip a | grep link/ether | awk '{print $2}'`")" \
+$'\n#Sudo:  ' `grep 'sudo ' /var/log/auth.log | wc -l`
+
+sudo crontab -u root -e
+
+The banner is optional. No error must be visible.
+		• The architecture of your operating system and its kernel version.
+		• The number of physical processors.
+		• The number of virtual processors.
+		
+		• The current available RAM on your server and its utilization rate as a percentage.
+		• The current available memory on your server and its utilization rate as a percentage.
+		• The current utilization rate of your processors as a percentage.
+		• The date and time of the last reboot.
+		• Whether LVM is active or not.
+		• The number of active connections.
+		• The number of users using the server.
+		• The IPv4 address of your server and its MAC (Media Access Control) address.
+		• The number of commands executed with the sudo program.
+
+Architecture: 
+CPU: 
+vCPU: 
+Memory: 
+Disk: 
+CPU: 
+Last: 
+LVM: 
+Connections: 
+User: 
+Network: 
+Sudo: 
