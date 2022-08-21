3e95ec5a9fa94dfc1ab89225d89a7deda0e3e71d
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-08-21 11:30:57 -0300

    So-long with images

--- so-long/src/main.c
+++ so-long/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 10:13:34 by mariana           #+#    #+#             */
-/*   Updated: 2022/08/21 10:20:15 by mariana          ###   ########.fr       */
+/*   Updated: 2022/08/21 11:27:00 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,61 +31,78 @@ typedef struct s_img
 	int		height;
 }	t_img;
 
+typedef struct s_sprites
+{
+	t_img	player;
+	t_img	wall;
+	t_img	empty_space;
+	t_img	collectible;
+	t_img	exit;
+}	t_sprites;
+
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	t_sprites sprites;
 }	t_data;
+
 
 int handle_destroy_window()
 {
 	exit(0);
 }
 
-int	handle_keyrelease(int keysym, t_data *data)
+int	handle_keyrelease(int keysym)
 {
 	if (keysym == XK_Escape)
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		exit(0);
 	return (0);
 }
 
-
-int	render()
+int	render(t_data *data)
 {
-	// char	*redDino = "./sprites/player/RedDino.xpm";
-	// t_img redDinoImg;
-	// if (data->win_ptr == NULL)
-	// 	return (1);
-	// // render_background(&data->img, WHITE_PIXEL);
-	// // render_rect(&data->img, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
-	// // 	100, 100, GREEN_PIXEL});
-	// // render_rect(&data->img, (t_rect){0, 0, 500, 300, RED_PIXEL});
-
-	// redDinoImg.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, redDino, &redDinoImg.width, &redDinoImg.height);
-	// redDinoImg.addr = mlx_get_data_addr(redDinoImg.mlx_img, &redDinoImg.bpp,
-	// 		&redDinoImg.line_len, &redDinoImg.endian);
-	// mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
-	// mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, redDinoImg.mlx_img, 15, 0);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->sprites.player.mlx_img, 32, 0);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->sprites.wall.mlx_img, 0, 0);
 
 	return (0);
+}
+
+void ft_create_imgs(t_data *data)
+{
+	char	*blueDino = "./sprites/player/BlueDino.xpm";
+	char	*title1 = "./sprites/background/titleBack1.xpm";
+	t_img blueDinoImg;
+	t_img title1Img;
+
+	blueDinoImg.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, blueDino, &blueDinoImg.width, &blueDinoImg.height);
+	blueDinoImg.addr = mlx_get_data_addr(blueDinoImg.mlx_img, &blueDinoImg.bpp, &blueDinoImg.line_len, &blueDinoImg.endian);
+
+	title1Img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr, title1, &title1Img.width, &title1Img.height);
+	title1Img.addr = mlx_get_data_addr(title1Img.mlx_img, &title1Img.bpp, &title1Img.line_len, &title1Img.endian);
+	data->sprites.player = blueDinoImg;
+	data->sprites.wall = title1Img;
 }
 
 int	main(void)
 {
-	t_data	data;
+	t_data		data;
 
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
+	
 	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
 	if (data.win_ptr == NULL)
 	{
 		free(data.win_ptr);
 		return (MLX_ERROR);
 	}
+	ft_create_imgs(&data);
+
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
-	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data); /* CHANGED */
-	mlx_hook(data.win_ptr, DestroyNotify, NoEventMask, &handle_destroy_window, &data); /* CHANGED */
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
+	mlx_hook(data.win_ptr, DestroyNotify, NoEventMask, &handle_destroy_window, &data);
 
 	mlx_loop(data.mlx_ptr);
 	mlx_destroy_display(data.mlx_ptr);
