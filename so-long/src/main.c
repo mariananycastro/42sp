38447a766556303cd8fa0e87f31b4ee8db5a7526
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-08-25 20:45:36 -0300

    Fix so long big leaks

--- so-long/src/main.c
+++ so-long/src/main.c
@@ -6,40 +6,38 @@
 /*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 10:13:34 by mariana           #+#    #+#             */
-/*   Updated: 2022/08/25 10:36:10 by mariana          ###   ########.fr       */
+/*   Updated: 2022/08/25 20:39:32 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void ft_set_map_matrix(t_data *data, const char *map_file)
+void	ft_set_map_matrix(t_data *data, const char *map_file)
 {
-	char	*map_matrix;
 	size_t	matrix_size;
 	char	buffer;
 	int		fd;
 	int		index;
 
-	matrix_size = data->map.height * data->map.width;
-	map_matrix = ft_calloc(matrix_size, sizeof(char));
-
+	matrix_size = (data->map.height * data->map.width) + 1;
+	data->map.matrix = ft_calloc(matrix_size, sizeof(char));
 	index = 0;
 	fd = open(map_file, O_RDONLY);
 	if (fd != -1)
 	{
 		while (read(fd, &buffer, 1) != 0)
 		{
-			if(buffer != '\n')
+			if (buffer != '\n')
 			{
-				map_matrix[index] = buffer;
-				if (buffer == 'P')
+				data->map.matrix[index] = buffer;
+				if (buffer == PLAYER)
 					data->map.player_position = index;
 				index++;
 			}
 		}
 	}
+	data->map.matrix[index] = '\0';
 	close(fd);
-	data->map.matrix = map_matrix;
 }
 
 int ft_validate_extension(const char *map_file)
@@ -66,6 +64,7 @@ int	ft_create_window(t_data	*data)
 	if (data->win_ptr == NULL)
 	{
 		free(data->win_ptr);
+		free(data->map.matrix);
 		return (MLX_ERROR);
 	}
 	return (0);
