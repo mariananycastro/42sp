67f2c4997a6e6e58d11c0a0d3c8a96cd8fff5733
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-09-03 11:53:38 -0300

    add mylib

---
+++ so-long/my_lib/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/12 19:04:46 by mariana           #+#    #+#             */
+/*   Updated: 2022/09/03 11:27:19 by mariana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <limits.h>
+#include "libft.h"
+
+int	main(void)
+{
+	int	x;
+	int	y;
+
+	x = 'c';
+	y = 'b';
+	x = ft_printf(" NULL %d NULL ", 'a');
+	y = printf(" NULL %d NULL ", 'a');
+	ft_printf("x = %d \n", x);
+	ft_printf("y = %d\n", y);
+	ft_printf("%d", ft_isalpha(48));
+	return (0);
+}
