7c36e24e76c023fd2d8fa0028ff49883d3573035
Mariana Yamada <mariana.yamada@rebase.com.br>
2022-09-25 12:05:45 -0300

    fix normi

--- pipex/main.c
+++ pipex/main.c
@@ -6,14 +6,10 @@
 /*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 17:00:04 by mariana           #+#    #+#             */
-/*   Updated: 2022/09/25 10:21:01 by mariana          ###   ########.fr       */
+/*   Updated: 2022/09/25 11:53:53 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
-// #include <sys/types.h>
-// #include <sys/stat.h>
-// #include <string.h>
 #include "pipex.h"
 
 int	ft_set_stdin(char *file)
@@ -42,7 +38,7 @@ int	ft_set_stdout(char *file2)
 	return (0);
 }
 
-int ft_exec_cmd(char *cmd, char *envp[])
+int	ft_exec_cmd(char *cmd, char *envp[])
 {
 	char	**array_arg_cmd;
 	int		process;
@@ -53,48 +49,57 @@ int ft_exec_cmd(char *cmd, char *envp[])
 	process = execve(p_args, array_arg_cmd, envp);
 	if (process < 0)
 		return (1);
-  return (0);
+	return (0);
+}
+
+void	ft_close_fd(int *fd)
+{
+	close(fd[0]);
+	close(fd[1]);
+}
+
+int	ft_exec_p1(char *argv[], int *fd, char *envp[])
+{
+	if (ft_set_stdin(argv[1]) != 0)
+		return (ERRINFILE);
+	dup2(fd[1], STDOUT_FILENO);
+	ft_close_fd(fd);
+	ft_exec_cmd(argv[2], envp);
+	return (0);
+}
+
+int	ft_exec_p2(char *argv[], int *fd, char *envp[], int argc)
+{
+	dup2(fd[0], STDIN_FILENO);
+	if (ft_set_stdout(argv[argc - 1]) != 0)
+		return (ERROUTFILE);
+	ft_close_fd(fd);
+	ft_exec_cmd(argv[3], envp);
+	return (0);
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	int		i;
-	char	**array_arg_cmd;
-	char	*p_args;
-	int		process;
+	int		fd[2];
+	int		pid2;
+	int		pid1;
 
 	if (argc < 4)
 		return (ERRARG);
-
-	i = 2;
-	int fd[2];
-    if (pipe(fd) == -1)
+	if (pipe(fd) == -1)
 		return (ERRPIPE);
-	int pid1 = fork();
-    if (pid1 < 0)
+	pid1 = fork();
+	if (pid1 == -1)
 		return (ERRFORK);
-	if (pid1 == 0) {
-		if (ft_set_stdin(argv[1]) != 0)
-			return (ERRINFILE);
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[0]);
-        close(fd[1]);
-		ft_exec_cmd(argv[2], envp);
-    }
-	int pid2 = fork();
-    if (pid2 < 0)
+	if (pid1 == 0)
+		ft_exec_p1(argv, fd, envp);
+	pid2 = fork();
+	if (pid2 == -1)
 		return (ERRFORK);
-    if (pid2 == 0) {
-        dup2(fd[0], STDIN_FILENO);
-		if (ft_set_stdout(argv[argc - 1]) != 0)
-			return (ERROUTFILE);
-        close(fd[0]);
-        close(fd[1]);
-        ft_exec_cmd(argv[3], envp);
-    }
-    close(fd[0]);
-    close(fd[1]);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
+	if (pid2 == 0)
+		ft_exec_p2(argv, fd, envp, argc);
+	ft_close_fd(fd);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (0);
 }
