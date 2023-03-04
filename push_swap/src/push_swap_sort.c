fc019148761350f81725701c8e995ceefd1ad2a5
Mariana Yamada <mariana.yamada@rebase.com.br>
2023-03-04 19:08:35 -0300

    fix sort 4

--- push_swap/src/push_swap_sort.c
+++ push_swap/src/push_swap_sort.c
@@ -6,7 +6,7 @@
 /*   By: mariana <mariana@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 20:44:28 by mariana           #+#    #+#             */
-/*   Updated: 2023/03/04 09:31:11 by mariana          ###   ########.fr       */
+/*   Updated: 2023/03/04 19:01:50 by mariana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,18 +65,22 @@ void	ft_sort_4(t_index_stack **stack)
 
 	stack_b = NULL;
 	if ((*stack)->next->position == 3)
-		ft_rotate_a(stack);
+		ft_swap_a(stack);
 	else if ((*stack)->previous->position == 3)
 		ft_reverse_rotate_a(stack);
 	else if ((*stack)->next->next->position == 3)
 	{
 		ft_reverse_rotate_a(stack);
-		ft_reverse_rotate_a(stack);
+		if (!ft_is_sorted(stack))	
+			ft_reverse_rotate_a(stack);
 	}
-	ft_push_b(&stack_b, stack);
-	ft_sort_3(stack);
-	ft_push_all(&stack_b, stack);
-	ft_rotate_a(stack);
+	if (!ft_is_sorted(stack))
+	{
+		ft_push_b(&stack_b, stack);
+		ft_sort_3(stack);
+		ft_push_all(&stack_b, stack);
+		ft_rotate_a(stack);
+	}
 }
 
 void	ft_sort_5(t_index_stack **stack)
@@ -86,6 +90,7 @@ void	ft_sort_5(t_index_stack **stack)
 
 	stack_b = NULL;
 	i = (*stack)->previous->index;
+	
 	while (i > 2)
 	{
 		if ((*stack)->position == 0 || (*stack)->position == 1)
